6059f0b4d753e64671245e11db7b20163370c16e
Joren Meynen <joren@farmdesk.eu>
2022-12-01 19:00:57 +0100

    day 1: using a comprehension

--- Day 1 - Calorie Counting/Day 1 - Calorie Counting.py
+++ Day 1 - Calorie Counting/Day 1 - Calorie Counting.py
@@ -1,18 +1,26 @@
-# %%
 with open('./input.txt') as f:
-    input = f.read().splitlines()
+    input = f.read().rstrip()
 
-# %%
-elves = []
-elf = 0
-for i in input:
-    if i == '':
-        elves.append(elf)
-        elf = 0
-    else:
-        elf += int(i)
-
-# %%
+groups = input.split('\n\n')
+elves = [sum(map(int, elf.split('\n'))) for elf in groups]
 elves.sort(reverse=True)
+
 print("1:", elves[0])
 print("2:", sum(elves[:3]))
+
+
+# Original
+# with open('./input.txt') as f:
+#     input = f.read().splitlines()
+
+# elves = []
+# elf = 0
+# for i in input:
+#     if i == '':
+#         elves.append(elf)
+#         elf = 0
+#     else:
+#         elf += int(i)
+
+# print("1:", elves[0])
+# print("2:", sum(elves[:3]))
