ce578f9f697a66a414df65101f7b5d81ad207e00
Joren Meynen <joren@farmdesk.eu>
2022-12-02 14:14:09 +0100

    renamed func and moved some code

--- Day 2 - Rock Paper Scissors/Day 2 - Rock Paper Scissors.py
+++ Day 2 - Rock Paper Scissors/Day 2 - Rock Paper Scissors.py
@@ -1,13 +1,8 @@
 with open('./input.txt') as f:
     input = f.read().rstrip().split('\n')
 
-score_hand = {'X': 1, 'Y': 2, 'Z': 3}
-score_round = {'win': 6, 'draw': 3, 'loss': 0}
 
-action = {'X': 'loss', 'Y': 'draw', 'Z': 'win'}
-
-
-def get_result_round_part_1(hand1, hand2):
+def get_result_round_p1(hand1, hand2):
     result = (ord(hand1) - ord(hand2)) % 3
     if result == 0:
         return 'win'
@@ -16,29 +11,31 @@ def get_result_round_part_1(hand1, hand2):
     else:
         return 'loss'
 
-def get_result_round_part_2(hand1, result):
+def get_hand_to_throw_p2(hand1, result):
     if result == 'win':
-        return chr((ord(hand1) - 1) % 3 + ord('X'))
+        offset_hand_to_throw = (ord(hand1) - 1) % 3
     elif result == 'draw':
-        return chr((ord(hand1) - 2) % 3 + ord('X'))
+        offset_hand_to_throw = (ord(hand1) - 2) % 3
     else:
-        return chr((ord(hand1) - 3) % 3 + ord('X'))
+        offset_hand_to_throw = (ord(hand1) - 3) % 3
+    return chr(offset_hand_to_throw + ord('X'))
 
-
+score_hand = {'X': 1, 'Y': 2, 'Z': 3}
+score_round = {'win': 6, 'draw': 3, 'loss': 0}
+action = {'X': 'loss', 'Y': 'draw', 'Z': 'win'}
 
 total_score_part_1 = []
 total_score_part_2 = []
 for line in input:
     i1, i2 = line.split(' ')
 
-    round_result_p1 = get_result_round_part_1(i1, i2)
+    round_result_p1 = get_result_round_p1(i1, i2)
     score_p1 = score_round[round_result_p1] + score_hand[i2]
     total_score_part_1.append(score_p1)
 
-    thrown_hand_p2 = get_result_round_part_2(i1, action[i2])
-    score_p2 = score_round[action[i2]] + score_hand[thrown_hand_p2]
+    hand_to_throw_p2 = get_hand_to_throw_p2(i1, action[i2])
+    score_p2 = score_round[action[i2]] + score_hand[hand_to_throw_p2]
     total_score_part_2.append(score_p2)
 
-    
 print(sum(total_score_part_1))
 print(sum(total_score_part_2))
