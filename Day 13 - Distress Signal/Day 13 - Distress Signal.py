561c02e28aba30200d6ee0f0ba6931583bf46bbf
Joren Meynen <joren@farmdesk.eu>
2022-12-14 02:31:39 +0100

    solution day 13

--- Day 13 - Distress Signal/Day 13 - Distress Signal.py
+++ Day 13 - Distress Signal/Day 13 - Distress Signal.py
@@ -6,6 +6,7 @@ with open('./input.txt') as f:
 def compareLists(list1, list2):
     # If list1 is smaller, or same length or shorter, return True
     # If list2 is smaller, or shorter, return False
+    val = None
 
     for i in range(len(list1)):
         sub1 = list1[i]
@@ -14,37 +15,62 @@ def compareLists(list1, list2):
             return False # list2 is shorter
 
         if type(sub1) == list and type(sub2) == list:
-            return compareLists(sub1, sub2)
+            val = compareLists(sub1, sub2)
         elif type(sub1) == list and type(sub2) != list:
-            return compareLists(sub1, [sub2])
+            val = compareLists(sub1, [sub2])
         elif type(sub1) != list and type(sub2) == list:
-            return compareLists([sub1], sub2)
-
-        if sub1 < sub2:
+            val = compareLists([sub1], sub2)
+        elif sub1 < sub2:
             return True  # el in list1 is smaller
         elif sub1 > sub2:
             return False # el in list2 is smaller
 
-    return True # list1 is shorter or equal
+        if val != None:
+            return val
+
+    if len(list1) < len(list2):
+        return True # list1 is shorter or equal
 
 
-def comparePair(pair):
-    e_1, e_2 = pair
-    return compareLists(e_1, e_2)
-
-correctly_ordered = []
+correctly_ordered_p1 = []
 import json
 for i in range(len(input)):
-    e_1, e_2 = [json.loads(x) for x in input[i].split('\n')]
-    correctly_ordered.append(compareLists(e_1, e_2))
-
+    pair = input[i]
+    e_1, e_2 = [json.loads(x) for x in pair.split('\n')]
+    correctly_ordered_p1.append(compareLists(e_1, e_2))
 solution_p1 = 0
-for i in range(len(correctly_ordered)):
-    if correctly_ordered[i]:
+for i in range(len(correctly_ordered_p1)):
+    if correctly_ordered_p1[i]:
         solution_p1 += i+1
-
-
 print("1:", solution_p1)
+
+def compareListSort(list1, list2):
+    result = compareLists(list1, list2)
+    if result:
+        return -1
+    else:
+        return 1
+
+from functools import cmp_to_key
+input_p2 = []
+for i in range(len(input)):
+    pair = input[i]
+    e_1, e_2 = [json.loads(x) for x in pair.split('\n')]
+    input_p2.append(e_1)
+    input_p2.append(e_2)
+
+divider_1 = [[2]]
+divider_2 = [[6]]
+
+input_p2.append(divider_1)
+input_p2.append(divider_2)
+
+input_p2.sort(key=cmp_to_key(compareListSort))
+solution_p2 = 1
+solution_p2 *= input_p2.index(divider_1) + 1
+solution_p2 *= input_p2.index(divider_2) + 1
+print("2:", solution_p2)
+
 
 
 
